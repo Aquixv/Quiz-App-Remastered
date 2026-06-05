870de482f322becc847275f20ae37454c0849405
unknown <dareakintundealamu@gmail.com>
2026-06-05 12:25:47 +0100

    Setup User Schema

--- Quiz-server/src/resolvers.ts
+++ Quiz-server/src/resolvers.ts
@@ -1,5 +1,26 @@
+import User from "../models/User";
+import Quiz from "../models/User";
+import Score from "../models/Score";
+
 export const resolvers = {
   Query: {
-    _empty: () => "Quiz API is alive and ready!",
+    getUsers: async () => {
+      return await User.find({});
+    },
+    getUser: async (_: any, { id }: { id: string }) => {
+      return await User.findById(id);
+    },
   },
+  
+  Mutation: {
+    registerUser: async (_: any, { username, password }: any) => {
+      const newUser = new User({ 
+        username, 
+        password,
+        totalPoints: 0 
+      });
+      
+      return await newUser.save();
+    }
+  }
 };
--- Quiz-server/src/typeDefs.ts
+++ Quiz-server/src/typeDefs.ts
@@ -1,5 +1,17 @@
 export const typeDefs = `#graphql
+  type User {
+    _id: ID!
+    username: String!
+    totalPoints: Int
+    createdAt: String
+  }
+
   type Query {
-    _empty: String
+    getUsers: [User]
+    getUser(id: ID!): User
+  }
+
+  type Mutation {
+    registerUser(username: String!, password: String!): User
   }
 `;
