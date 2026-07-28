868089c38032da12475df10eac313c0e8b62056d
unknown <dareakintundealamu@gmail.com>
2026-07-28 20:57:24 +0100

    fixed points

--- aquiz-app/src/Pages/Profile.tsx
+++ aquiz-app/src/Pages/Profile.tsx
@@ -163,7 +163,7 @@ const Profile = () => {
 
         <div className="grid grid-cols-2 gap-4 mb-8">
           <div className="glass-card p-6 rounded-3xl text-center border border-white/5">
-            <span className="text-electric-violet text-3xl font-black block">{userStats.calculatedTotalPoints.toLocaleString()}</span>
+            <span className="text-electric-violet text-3xl font-black block">{calculatedTotalPoints.toString()}</span>
             <span className="text-[10px] uppercase font-bold tracking-widest opacity-50">Total Points</span>
           </div>
           <div className="glass-card p-6 rounded-3xl text-center border border-white/5">
